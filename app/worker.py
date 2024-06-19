3264f724fe102ce594da6721f25ffd6ce4f37c63
Andrew Tork Baker <andrew.tork.baker@gmail.com>
2024-06-18 17:52:02 -0700

    Celery tweaks for Windows

--- app/worker.py
+++ app/worker.py
@@ -3,5 +3,5 @@ from config import celery_app
 # https://stackoverflow.com/questions/67023208/run-celery-worker-with-a-compiled-python-module-compiled-using-pyinstaller
 if __name__ == '__main__':
     celery_app.worker_main(
-        argv=['worker', '--beat', '--loglevel=info', '--concurrency=1']
+        argv=['worker', '--loglevel=info', '--pool=threads', '--concurrency=1']
     )
