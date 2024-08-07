f597bedef796e55fd37f6befe851e8e1eff0a6bd
Andrew Tork Baker <andrew.tork.baker@gmail.com>
2024-08-06 19:51:11 -0700

    Ignore browser extension source when building Electron app

--- forge.config.js
+++ forge.config.js
@@ -11,6 +11,9 @@ module.exports = {
       // Python source and build code
       'speck-server',
 
+      // Extension
+      'extension',
+
       // Miscellaneous
       '.gitignore',
       '.vscode',
